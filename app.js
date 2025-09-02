9f12a68ed9fa79f60aa54640926353f921f0ab5d
Zahid Shaikh <mrzerrry@gmail.com>
2025-09-02 23:03:16 +0545

    added card's of categories.

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const list = document.getElementById("trending-list");
-  list.innerHTML = `
-    <li>🚀 GitHub Copilot is trending</li>
-    <li>📱 iPhone 16 launch rumors</li>
-    <li>🎬 New Netflix show release</li>
-  `;
-});
+const categories = [
+  { name: "Tech", image: "assets/img/Tech.jpg" },
+  { name: "Sports", image: "assets/img/sports.jpg" },
+  { name: "Entertainment", image: "assets/img/entertainment.jpg" },
+  { name: "Finance", image: "assets/img/finance.jpg" },
+  { name: "AI", image: "assets/img/Ai.jpg" }
+];
+  const cardsContainer = document.querySelector(".cards-container");
+
+  categories.forEach(cat => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `
+      <img src="${cat.image}" alt="${cat.name}">
+      <div class="card-body">
+        <h3>${cat.name}</h3>
+        <button onclick="location.href='category.html?type=${cat.name.toLowerCase()}'">Explore</button>
+      </div>
+    `;
+    cardsContainer.appendChild(card);
+  });
+}); 
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const body = document.body;
